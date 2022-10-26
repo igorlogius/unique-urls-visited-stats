890ca3649614174b15b8aa6ba9a52a7555bfaf6a
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-10-26 14:46:59 +0200

    feat: adds a couple of more days fix: display not since ... but only the visits of the day

--- popup.js
+++ popup.js
@@ -4,40 +4,50 @@ const today = document.getElementById('today');
 const yesterday = document.getElementById('yesterday');
 const twodaysago = document.getElementById('twodaysago');
 const threedaysago = document.getElementById('threedaysago');
+const fourdaysago = document.getElementById('fourdaysago');
+const fivedaysago = document.getElementById('fivedaysago');
+const sixdaysago = document.getElementById('sixdaysago');
+const sevendaysago = document.getElementById('sevendaysago');
 
-async function getVisitsCountSinceBeginningOfXDaysAgo(X){
-    let d = new Date();
-    d.setDate(d.getDate() - X); // back in time
-    d.setHours(0,0,0,0);  // set to midnight
-    const startTime = d.getTime();
-    return getVisitsCountSince(startTime);
+async function getVisitsCountXDaysAgo(X){
+    // go to the day we are interested in
+    let start = new Date();
+    start.setDate(start.getDate() - X); // back in time
+    start.setHours(0,0,0,0);  // set to midnight
+
+    let end = new Date();
+    if(X > 0){
+         // to next days beginning
+        end.setDate(start.getDate() + 1);
+        end.setHours(0,0,0,0);
+    }
+    return getVisitsCountInRange(start,end);
 }
 
 // startTime Epoch
-async function getVisitsCountSince(startTime) {
-
+async function getVisitsCountInRange(startTime,endTime) {
     const uniqURLs = new Set();
-    const endTime = Date.now();
-    //console.debug('Hours: ', (endTime-startTime)/1000/60/60);
-
-    const uniq_visit_history_items = (await browser.history.search({
+    (await browser.history.search({
+        maxResults:Number.MAX_SAFE_INTEGER, // can not be set to -1 so we use a very high number
         text: "",  // retrieve all history.HistoryItem objects that meet all the other criteria
         startTime,
         endTime
-    })).filter( (element) => {
-        if ( uniqURLs.has(element.url) ){
-            return false;
+    })).forEach( (item) => {
+        if ( !uniqURLs.has(item.url) ){
+            uniqURLs.add(item.url);
         }
-        uniqURLs.add(element.url);
-        return true;
     });
-    return uniq_visit_history_items.length;
+    return uniqURLs.size;
 }
 
 (async () => {
-    today.innerText = await getVisitsCountSinceBeginningOfXDaysAgo(0);
-    yesterday.innerText = await getVisitsCountSinceBeginningOfXDaysAgo(1);
-    twodaysago.innerText = await getVisitsCountSinceBeginningOfXDaysAgo(2);
-    threedaysago.innerText = await getVisitsCountSinceBeginningOfXDaysAgo(2);
+    today.innerText = await getVisitsCountXDaysAgo(0);
+    yesterday.innerText = await getVisitsCountXDaysAgo(1);
+    twodaysago.innerText = await getVisitsCountXDaysAgo(2);
+    threedaysago.innerText = await getVisitsCountXDaysAgo(3);
+    fourdaysago.innerText = await getVisitsCountXDaysAgo(4);
+    fivedaysago.innerText = await getVisitsCountXDaysAgo(5);
+    sixdaysago.innerText = await getVisitsCountXDaysAgo(6);
+    sevendaysago.innerText = await getVisitsCountXDaysAgo(7);
 })();
 
